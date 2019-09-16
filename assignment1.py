a67a8265c6400becfe1ec103568ca8eba2a03785
BBBrando <31227156+BBBrando@users.noreply.github.com>
2019-09-15 19:47:02 -0700

    Add files via upload

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,27 @@
 
 #Part 1 of assignment
 def stringCount(aList):
+
+
+    wordCount = dict()
+    for word in aList:
+        if word in wordCount:
+            wordCount[word] += 1
+        else:
+            wordCount[word] = 1
+
+    wordList = list(wordCount.keys())
+    wordList = sorted(wordList, key=lambda s: s.lower())
+    i = 0
+    for word in wordCount:
+        print(wordList[i], " ", wordCount[word])
+        i=i+1
     return None
 
 #Part 2 of assignment
 def isFloat(value):
-    return False
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
